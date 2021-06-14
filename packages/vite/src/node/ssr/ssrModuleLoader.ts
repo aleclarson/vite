f8d3b9efacdd3b023f9376c050b3ceb1241df0f0
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-06-14 18:32:08 -0400

    fix: skip `ssrRewriteStacktrace` if it fails

--- packages/vite/src/node/ssr/ssrModuleLoader.ts
+++ packages/vite/src/node/ssr/ssrModuleLoader.ts
@@ -207,7 +207,9 @@ async function instantiateModule(
       })
     }
   } catch (e) {
-    rebindErrorStacktrace(e, ssrRewriteStacktrace(e, moduleGraph))
+    try {
+      rebindErrorStacktrace(e, ssrRewriteStacktrace(e, moduleGraph))
+    } catch {}
     logger.error(`Error when evaluating SSR module ${url}:\n\n${e.stack}`, {
       timestamp: true,
       clear: server.config.clearScreen,
