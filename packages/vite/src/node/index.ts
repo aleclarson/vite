ecb1ca7a81822ea74676b9640af5e56c18db300d
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-06-26 16:10:28 -0400

    feat: expose `applyHtmlTransforms` and `resolveHtmlTransforms`

--- packages/vite/src/node/index.ts
+++ packages/vite/src/node/index.ts
@@ -7,6 +7,7 @@ export { send } from './server/send'
 export { createLogger } from './logger'
 export { transformWithEsbuild } from './plugins/esbuild'
 export { resolvePackageData, resolvePackageEntry } from './plugins/resolve'
+export { applyHtmlTransforms, resolveHtmlTransforms } from './plugins/html'
 export { normalizePath } from './utils'
 
 // additional types
