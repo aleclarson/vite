c32c5ac5c45ffb17e8f37559fc19af69afef4662
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-07-24 22:11:47 -0400

    fix: handle when `base` is an external URL

--- packages/vite/src/node/plugins/importAnalysis.ts
+++ packages/vite/src/node/plugins/importAnalysis.ts
@@ -90,7 +90,7 @@ function markExplicitImport(url: string) {
  */
 export function importAnalysisPlugin(config: ResolvedConfig): Plugin {
   const { root, base } = config
-  const clientPublicPath = path.posix.join(base, CLIENT_PUBLIC_PATH)
+  const clientPublicPath = base + CLIENT_PUBLIC_PATH.slice(1)
 
   let server: ViteDevServer
 
