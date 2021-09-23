c17101376c13cdf07c14b8bcfd7242efd339ae09
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-09-23 14:24:36 -0400

    fix: pass `followSymlinks: false` to chokidar
    Chokidar does not play nice with circular symlinks, and following symlinks doesn't matter anyway,
    since Vite explicitly watches any modules used by the application.

--- packages/vite/src/node/server/index.ts
+++ packages/vite/src/node/server/index.ts
@@ -346,7 +346,8 @@ export async function createServer(
     ignoreInitial: true,
     ignorePermissionErrors: true,
     disableGlobbing: true,
-    ...watchOptions
+    ...watchOptions,
+    followSymlinks: false
   }) as FSWatcher
 
   const plugins = config.plugins
