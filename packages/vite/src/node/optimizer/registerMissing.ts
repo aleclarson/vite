28964f8d9616b218aaef666e008155d6e21db9c4
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-10-01 03:47:22 -0400

    fix(optimizer): avoid overwriting `server._pendingReload`
    We don't want `viteTransformMiddleware` to wait on a promise that never resolves.

--- packages/vite/src/node/optimizer/registerMissing.ts
+++ packages/vite/src/node/optimizer/registerMissing.ts
@@ -98,7 +98,7 @@ export function createMissingImporterRegisterFn(
       currentMissing[id] = resolved
       if (handle) clearTimeout(handle)
       handle = setTimeout(() => rerun(ssr), debounceMs)
-      server._pendingReload = new Promise((r) => {
+      server._pendingReload ||= new Promise((r) => {
         pendingResolve = r
       })
     }
