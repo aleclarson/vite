48dfdbeb2b72c943eb76333530c9dbb3cbd4a63f
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-10-06 17:58:50 -0400

    refactor: simplify `resolveSSRExternal`
    - add `collectExternals` function for recursive tracing
    - apply `config.ssr` just once, after all externals are collected
    - support packages with deep imports but no main entry
    - resolve dependencies of linked packages correctly

--- packages/vite/src/node/optimizer/registerMissing.ts
+++ packages/vite/src/node/optimizer/registerMissing.ts
@@ -55,10 +55,7 @@ export function createMissingImporterRegisterFn(
       knownOptimized = newData!.optimized
 
       // update ssr externals
-      server._ssrExternals = resolveSSRExternal(
-        server.config,
-        Object.keys(knownOptimized)
-      )
+      server._ssrExternals = resolveSSRExternal(server.config)
 
       logger.info(
         chalk.greenBright(`✨ dependencies updated, reloading page...`),
