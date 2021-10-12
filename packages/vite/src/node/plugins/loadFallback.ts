354c75c4855bc9ab82d0c518c16209d27fa1cca3
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-10-12 14:45:02 -0400

    feat: set `meta.filename` for modules loaded from disk
    This provides an efficient heuristic for knowing if a module is virtual.

--- packages/vite/src/node/plugins/loadFallback.ts
+++ packages/vite/src/node/plugins/loadFallback.ts
@@ -9,11 +9,9 @@ export function loadFallbackPlugin(): Plugin {
   return {
     name: 'vite:load-fallback',
     async load(id) {
-      try {
-        return fs.readFile(cleanUrl(id), 'utf-8')
-      } catch (e) {
-        return fs.readFile(id, 'utf-8')
-      }
+      const filename = cleanUrl(id)
+      const code = await fs.readFile(filename, 'utf-8')
+      return { code, meta: { filename } }
     }
   }
 }
