f3492715b24c83bc2197e11257e1566a49c71037
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-10-22 18:12:42 -0400

    fix(server): use `options` argument in caching of `transformRequest` calls

--- packages/vite/src/node/server/index.ts
+++ packages/vite/src/node/server/index.ts
@@ -326,7 +326,7 @@ export interface ViteDevServer {
   /**
    * @internal
    */
-  _pendingRequests: Record<string, Promise<TransformResult | null> | null>
+  _pendingRequests: Map<string, Promise<TransformResult | null>>
 }
 
 export async function createServer(
@@ -417,7 +417,7 @@ export async function createServer(
     _isRunningOptimizer: false,
     _registerMissingImport: null,
     _pendingReload: null,
-    _pendingRequests: Object.create(null)
+    _pendingRequests: new Map()
   }
 
   server.transformIndexHtml = createDevHtmlTransformFn(server)
