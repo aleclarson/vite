f73d232d3aaa86d6b267b4ea09d104216c7e8f49
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-12-08 21:46:24 -0500

    fix: update `cacheDir` usage
    See 1f723c23b6aa81c3e10faee043a21cb638d926e3

--- packages/vite/src/node/http.ts
+++ packages/vite/src/node/http.ts
@@ -9,8 +9,8 @@ import { Logger } from './logger'
 
 export interface CommonServerOptions {
   /**
-   * Specify server port. Note if the port is already being used, Vite will 
-   * automatically try the next available port so this may not be the actual 
+   * Specify server port. Note if the port is already being used, Vite will
+   * automatically try the next available port so this may not be the actual
    * port the server ends up listening on.
    */
   port?: number
@@ -19,7 +19,7 @@ export interface CommonServerOptions {
    */
   strictPort?: boolean
   /**
-   * Specify which IP addresses the server should listen on. 
+   * Specify which IP addresses the server should listen on.
    * Set to 0.0.0.0 to listen on all addresses, including LAN and public addresses.
    */
   host?: string | boolean
@@ -67,7 +67,7 @@ export interface CommonServerOptions {
 /**
  * https://github.com/expressjs/cors#configuration-options
  */
- export interface CorsOptions {
+export interface CorsOptions {
   origin?:
     | CorsOrigin
     | ((origin: string, cb: (err: Error, origins: CorsOrigin) => void) => void)
@@ -107,7 +107,7 @@ export async function resolveHttpServer(
 
 export async function resolveHttpsConfig(
   https?: boolean | HttpsServerOptions,
-  cacheDir?: string
+  cacheDir?: string | false
 ): Promise<HttpsServerOptions | undefined> {
   if (!https) return undefined
 
@@ -211,7 +211,7 @@ async function createCertificate() {
   return pems.private + pems.cert
 }
 
-async function getCertificate(cacheDir?: string) {
+async function getCertificate(cacheDir?: string | false) {
   if (!cacheDir) return await createCertificate()
 
   const cachePath = path.join(cacheDir, '_cert.pem')
