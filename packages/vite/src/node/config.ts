5b8b46799afbf173e0a8a6a38700c9e2fa9eba85
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-12-21 02:18:39 -0500

    fix: strange "Assigning to rvalue" syntax bug

--- packages/vite/src/node/config.ts
+++ packages/vite/src/node/config.ts
@@ -264,7 +264,7 @@ export async function resolveConfig(
   // production-specific behavior, so set it here even though we haven't
   // resolve the final mode yet
   if (mode === 'production') {
-    process.env.NODE_ENV = 'production'
+    process.env['NODE_ENV'] = 'production'
   }
 
   const configEnv = {
@@ -347,7 +347,7 @@ export async function resolveConfig(
   const isProduction = (process.env.VITE_USER_NODE_ENV || mode) === 'production'
   if (isProduction) {
     // in case default mode was not production and is overwritten
-    process.env.NODE_ENV = 'production'
+    process.env['NODE_ENV'] = 'production'
   }
 
   // resolve public base url
@@ -1078,7 +1078,7 @@ export function loadEnv(
           env[key] = value
         } else if (key === 'NODE_ENV') {
           // NODE_ENV override in .env file
-          process.env.VITE_USER_NODE_ENV = value
+          process.env['VITE_USER_NODE_ENV'] = value
         }
       }
     }
