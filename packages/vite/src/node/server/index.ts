069d9d158a12745ae5269903ac2608538c2d40f4
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-09-25 12:17:54 -0400

    feat: support `import.meta.hot.invalidate`
    Self-accepting modules can call this to continue propagating the HMR update to importers. Conditional `hot.accept` calls do not work unless `hot.invalidate` is called when the condition fails.

--- packages/vite/src/node/server/index.ts
+++ packages/vite/src/node/server/index.ts
@@ -64,9 +64,9 @@ import {
   serveStaticMiddleware
 } from './middlewares/static'
 import { timeMiddleware } from './middlewares/time'
-import { ModuleGraph } from './moduleGraph'
+import { ModuleGraph, ModuleNode } from './moduleGraph'
 import { errorMiddleware, prepareError } from './middlewares/error'
-import type { HmrOptions } from './hmr'
+import { HmrOptions, updateModules } from './hmr'
 import { handleFileAddUnlink, handleHMRUpdate } from './hmr'
 import { openBrowser } from './openBrowser'
 import type { TransformOptions, TransformResult } from './transformRequest'
@@ -487,6 +487,21 @@ export async function createServer(
   })
   watcher.on('unlink', (file) => {
     handleFileAddUnlink(normalizePath(file), server)
+  })
+
+  ws.on('custom', ({ event, data: file }: { event: string; data: any }) => {
+    if (event === 'vite:invalidate') {
+      const modules = moduleGraph.getModulesByFile(file)
+      if (modules) {
+        const importers = new Set<ModuleNode>()
+        for (const mod of modules) {
+          for (const importer of mod.importers) {
+            importers.add(importer)
+          }
+        }
+        updateModules(file, Array.from(importers), Date.now(), server)
+      }
+    }
   })
 
   if (!middlewareMode && httpServer) {
